276c9ca2a6b1f156182956a6c4a8a7af0f2561a3
Tanishka sonavane <76207294+Tanishka-s@users.noreply.github.com>
2024-01-17 18:06:32 +0530

    Add files via upload

---
+++ DemoIsTheSiteUpApplicationTests.java
@@ -0,0 +1,13 @@
+package id.demoisthesiteup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoIsTheSiteUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
